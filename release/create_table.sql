45407850c893f773a2c68c0aa6a29c43cc0f5557
v_pijiang <aaa>
2021-03-17 17:21:15 +0800

    手写JPA

---
+++ release/create_table.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE `t_jpa_user` (
+  `name` varchar(255) DEFAULT NULL COMMENT '名字',
+  `age` tinyint(4) DEFAULT NULL COMMENT '年龄'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/java/com/aurora/happy/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.aurora.happy.mapper;
+
+import com.aurora.happy.pojo.User;
+
+/**
+ * Created by pijiang on 2021/3/17.
+ */
+public class UserMapper extends BaseMapper<User>{
+
+    @Override
+    public int add(User bean) {
+        return super.add(bean);
+    }
+}
