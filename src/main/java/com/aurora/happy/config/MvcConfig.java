f347c4414079a758936909163970e47fa70f3c9e
v_pijiang <aaa>
2021-05-13 18:12:35 +0800

    简单鉴权登陆

--- src/main/java/com/aurora/happy/config/MvcConfig.java
+++ src/main/java/com/aurora/happy/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.aurora.happy.config;
 
-import com.aurora.happy.interceptor.BizInterceptor;
 import com.aurora.happy.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,9 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new BizInterceptor()).addPathPatterns("/**")
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/doc.html/**"); // 多加一个路径排除
-
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
     }
 }
