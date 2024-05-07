161f37541a23f8bedf1427f3b0cf929ee29b0299
Aurora <592706328@qq.com>
2024-05-07 23:16:34 +0800

    日历时钟 & 快捷键

--- src/main/java/com/aurora/happy/swing/ToolBox.java
+++ src/main/java/com/aurora/happy/swing/ToolBox.java
@@ -1,5 +1,6 @@
 package com.aurora.happy.swing;
 
+import cn.hutool.core.date.ChineseDate;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import javafx.util.Pair;
@@ -10,29 +11,36 @@ import org.dom4j.io.XMLWriter;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.Element;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Timer;
+import java.util.*;
 
 /**
  * 迷你工具箱
  */
 public class ToolBox {
-    private static final String TO_XML_ESCAPE = "toXmlEscape";
-    private static final String TO_JSON_ESCAPE = "toJsonEscape";
-    private static final String TO_XML_PRETTY = "toXmlPretty";
-    private static final String TO_JSON_PRETTY = "toJsonPretty";
+    private static final String TO_XML_ESCAPE = "toXmlEscape",
+                                TO_JSON_ESCAPE = "toJsonEscape",
+                                TO_XML_PRETTY = "toXmlPretty",
+                                TO_JSON_PRETTY = "toJsonPretty";
 
     public static void main(String[] args) {
         //事件调度线程，保证UI操作线程安全
@@ -72,33 +80,36 @@ public class ToolBox {
         tabPanel.addTab("json格式化", getTabPanel(TO_JSON_PRETTY, left4, right4));
         mainPanel.add(tabPanel, BorderLayout.CENTER);
 
-        //top画板 - 按钮
+        //按钮
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 5));  //右对齐，按钮间5像素间隔
         AbstractButton prettyButton = getButton("美化", true);
         AbstractButton btn1 = getButton("转换", false);
         AbstractButton btn2 = getButton("复制", false);
         AbstractButton btn3 = getButton("截图", false);
         AbstractButton cleanButton = getButton("清空", false);
+        btn1.setToolTipText("CTRL+ENTER");  //快捷键提示
+        prettyButton.setSelected(true);  //默认美化
         topPanel.add(prettyButton);
         topPanel.add(btn1);
         topPanel.add(btn2);
         topPanel.add(btn3);
         topPanel.add(cleanButton);
-        mainPanel.add(topPanel, BorderLayout.NORTH);
 
-        //bottom画板 - 错误回显
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        //bottom
+        JPanel buttomPanel = new JPanel(new BorderLayout());
         JLabel errLable = new JLabel("");
         errLable.setForeground(Color.RED);
-        bottomPanel.add(errLable);
-        bottomPanel.setPreferredSize(new Dimension(bottomPanel.getPreferredSize().width, 20));  //只设置高度
-        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+        buttomPanel.add(errLable, BorderLayout.WEST);
+        buttomPanel.setPreferredSize(new Dimension(buttomPanel.getPreferredSize().width, 20));  //只设置高度
+        addClock(buttomPanel);  //time
 
         //整合
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(buttomPanel, BorderLayout.SOUTH);
         frame.add(mainPanel);
         frame.setVisible(true);     //可见
 
-        //事件-清空
+        //清空
         cleanButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -107,47 +118,14 @@ public class ToolBox {
                 pair.getValue().setText(null); //右文本域
             }
         });
-        //事件-转换
+        //转换
         btn1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    errLable.setText(null); //清空错误栏
-                    boolean isPretty = prettyButton.isSelected();
-                    String tabName = tabPanel.getSelectedComponent().getName();
-                    Pair<JTextArea, JTextArea> pair = areaList.get(tabPanel.getSelectedIndex());
-                    JTextArea leftArea = pair.getKey();
-                    JTextArea rightArea = pair.getValue();
-                    String oldText = leftArea.getText();
-                    String newText = "";
-                    switch (tabName){
-                        case TO_XML_ESCAPE:
-                            oldText = toXmlPretty(oldText, isPretty); //是否一行
-                            newText = toXmlEscape(oldText);
-                            break;
-                        case TO_XML_PRETTY:
-                            newText = toXmlPretty(oldText, isPretty);
-                            break;
-                        case TO_JSON_ESCAPE:
-                            oldText = toJsonPretty(oldText, isPretty);
-                            newText = toJsonEscape(oldText);
-                            break;
-                        case TO_JSON_PRETTY:
-                                newText = toJsonPretty(oldText, isPretty);
-                            break;
-                        default:
-                            throw new RuntimeException("未知操作类型！");
-                    }
-                    rightArea.setText(newText);
-                } catch (Exception ex) {
-                    String errMessage = "错误提示: " + ex.getMessage();
-                    errLable.setText(errMessage);
-                    errLable.setToolTipText(errMessage);   //鼠标悬浮提示
-                    ex.printStackTrace();
-                }
+                doSubmit(errLable, prettyButton, tabPanel, areaList);
             }
         });
-        //事件-复制
+        //复制
         btn2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -166,7 +144,7 @@ public class ToolBox {
                 JOptionPane.showMessageDialog(null, "已复制", "通知", JOptionPane.INFORMATION_MESSAGE);
             }
         });
-        //事件-截图
+        //截图
         btn3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -174,9 +152,136 @@ public class ToolBox {
             }
         });
 
+        //快捷键
+        bindKeyBoard(areaList, tabPanel, prettyButton, errLable);  //初始tab绑定
+        tabPanel.addChangeListener(new ChangeListener() {
+            //切换重新加载绑定
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                bindKeyBoard(areaList, tabPanel, prettyButton, errLable);
+            }
+        });
+
         //整合
         frame.setContentPane(mainPanel);  //设置为容器
         frame.setVisible(true); //是否可见
+    }
+
+    /**
+     * 快捷键绑定
+     * @param areaList
+     * @param tabPanel
+     * @param prettyButton
+     * @param errLable
+     */
+    private static void bindKeyBoard(List<Pair<JTextArea, JTextArea>> areaList, JTabbedPane tabPanel, AbstractButton prettyButton, JLabel errLable) {
+        Pair<JTextArea, JTextArea> pair = areaList.get(tabPanel.getSelectedIndex());
+        JTextArea leftArea = pair.getKey();
+        //ctrl+enter 转换
+        leftArea.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, KeyEvent.CTRL_DOWN_MASK), "submit");
+        leftArea.getActionMap().put("submit", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                doSubmit(errLable, prettyButton, tabPanel, areaList);
+            }
+        });
+        //撤销管理器
+        UndoManager undoManager = new UndoManager();
+        leftArea.getDocument().addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                undoManager.addEdit(e.getEdit());
+            }
+        });
+        //ctrl+z 撤销
+        leftArea.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "undo");
+        leftArea.getActionMap().put("undo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (undoManager.canUndo()){
+                    undoManager.undo();
+                }
+            }
+        });
+        //ctrl+shift+z 反撤销
+        leftArea.getInputMap().put(KeyStroke.getKeyStroke("ctrl shift Z"), "redo");
+        leftArea.getActionMap().put("redo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (undoManager.canRedo()){
+                    undoManager.redo();
+                }
+            }
+        });
+        //ctrl+d 复制当前行
+        leftArea.getInputMap().put(KeyStroke.getKeyStroke("ctrl D"), "copyLine");
+        leftArea.getActionMap().put("copyLine", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int position = leftArea.getCaretPosition();                                         //光标位置
+                    Element rootElement = leftArea.getDocument().getDefaultRootElement();
+                    int lineNum = rootElement.getElementIndex(position);                                //光标所在行
+                    Element lineEle = rootElement.getElement(lineNum);                                  //行元素
+                    int start = lineEle.getStartOffset();                                               //行头
+                    int end = lineEle.getEndOffset();                                                   //行尾
+                    String lineText = leftArea.getDocument().getText(start, end - start);       //行内容
+                    int nextLineStart = (lineNum <rootElement.getElementCount() - 1)
+                            ? rootElement.getElement(lineNum + 1).getStartOffset()
+                            : leftArea.getDocument().getLength() + 1;                   //下一行行头
+                    leftArea.getDocument().insertString(nextLineStart, lineText, null);             //在下一行追加内容
+                } catch (Exception ex) {
+                    String errMessage = "错误提示: " + ex.getMessage();
+                    errLable.setText(errMessage);
+                    errLable.setToolTipText(errMessage);   //鼠标悬浮提示
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * 转换提交
+     * @param errLable
+     * @param prettyButton
+     * @param tabPanel
+     * @param areaList
+     */
+    private static void doSubmit(JLabel errLable, AbstractButton prettyButton, JTabbedPane tabPanel, List<Pair<JTextArea, JTextArea>> areaList) {
+        try {
+            errLable.setText(null); //清空错误栏
+            boolean isPretty = prettyButton.isSelected();
+            String tabName = tabPanel.getSelectedComponent().getName();
+            Pair<JTextArea, JTextArea> pair = areaList.get(tabPanel.getSelectedIndex());
+            JTextArea leftArea = pair.getKey();
+            JTextArea rightArea = pair.getValue();
+            String oldText = leftArea.getText();
+            String newText = "";
+            switch (tabName){
+                case TO_XML_ESCAPE:
+                    oldText = toXmlPretty(oldText, isPretty); //是否一行
+                    newText = toXmlEscape(oldText);
+                    break;
+                case TO_XML_PRETTY:
+                    newText = toXmlPretty(oldText, isPretty);
+                    break;
+                case TO_JSON_ESCAPE:
+                    oldText = toJsonPretty(oldText, isPretty);
+                    newText = toJsonEscape(oldText);
+                    break;
+                case TO_JSON_PRETTY:
+                    newText = toJsonPretty(oldText, isPretty);
+                    break;
+                default:
+                    throw new RuntimeException("未知操作类型！");
+            }
+            rightArea.setText(newText);
+        } catch (Exception ex) {
+            String errMessage = "错误提示: " + ex.getMessage();
+            errLable.setText(errMessage);
+            errLable.setToolTipText(errMessage);   //鼠标悬浮提示
+            ex.printStackTrace();
+        }
     }
 
     /**
@@ -297,4 +402,24 @@ public class ToolBox {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 时钟
+     * @param buttomPanel
+     */
+    private static void addClock(JPanel buttomPanel){
+        buttomPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+        JLabel timeLabel = new JLabel("", SwingConstants.CENTER);
+        timeLabel.setFont(new Font("宋体", Font.ITALIC, 12));
+        buttomPanel.add(timeLabel, BorderLayout.EAST);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                String format = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss E")); //E表示周
+                String chnMonth = new ChineseDate(new Date()).toString().split(" ")[1]; //获取农历月份
+                timeLabel.setText(format.concat(" ").concat(chnMonth));
+            }
+        }, 0, 1_000);
+    }
 }
+
